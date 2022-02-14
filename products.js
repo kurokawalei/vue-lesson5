e4e5086d3bc9acb9b3c9ff954279362f0a28048f
kurokawa <kurokawa2011@gmail.com>
2022-02-14 17:56:39 +0800

    第二次上傳

--- products.js
+++ products.js
@@ -1,11 +1,12 @@
 
-    
      
       import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.26/vue.esm-browser.min.js';
-    
       import pageinfo from './page.js';
 
-    const app = createApp( {
+
+
+
+       const app = createApp( {
 
         data(){
             
@@ -15,7 +16,9 @@
                 apiPach:'kurokawa2021',
                 prList:[],
                 pagination:{},
-                carData:{}
+                carData:{},
+                productId:'',
+                isloading:false,
     
             }
         },
@@ -23,9 +26,7 @@
     
           //  https://vue3-course-api.hexschool.io/v2/api/kurokawa2021/products/all
     
-            getProductList( page = 1 ){
-    
-    
+            getProductList( page = 1 ){   
                 axios.get(`${this.apiUrl}/v2/api/${this.apiPach}/products/?page=${page}`)
                 .then( (res) => {
     
@@ -36,8 +37,130 @@
                 })
                 .catch((er) => {
                     console.log(er);
-                } )
+                });
     
+            },
+
+            getCarsDataList(){
+
+               // https://vue3-course-api.hexschool.io/v2/api/kurokawa2021/cart
+
+                axios.get(`${this.apiUrl}/v2/api/${this.apiPach}/cart`)
+                .then( (res) => {
+                 
+                   this.carData = res.data.data;
+                   console.log( res );
+                    
+                    
+                })
+                .catch((er) => {
+                    console.log(er);
+                });
+
+
+            },
+
+            openProductModal(id){
+
+                this.productId = id;
+                this.$refs.productModal.opentheModalfn();
+
+
+            },
+
+            addToCar(id , qty = 1 ){
+              
+                    //https://vue3-course-api.hexschool.io/v2/api/kurokawa2021/cart
+
+                    const data = {
+
+                        product_id:id,
+                        qty,
+
+                    };
+
+                    this.isloading = id;
+
+
+                    axios.post(`${this.apiUrl}/v2/api/${this.apiPach}/cart` , {data})
+                    .then( (res) => {
+        
+                    
+                     this.getCarsDataList();
+                     this.isloading = '';
+
+                    })
+                    .catch((er) => {
+                        console.log(er);
+                    });
+
+            },
+
+            upLoadItem(item){
+
+              
+
+
+                const data = {
+
+                    product_id: item.id,
+                    qty: item.qty
+                };
+
+                this.isloading = item.id;
+
+              
+
+                axios.put(`${this.apiUrl}/v2/api/${this.apiPach}/cart/${item.id}` , {data} )
+                .then( (res) => {
+                    console.log(res);
+                    this.getCarsDataList();       
+                    this.isloading = '';             
+                })
+                .catch((er) => {
+                    console.log(er);
+                });
+                
+
+            },
+
+
+            removeItem(id){
+
+
+                axios.delete(`${this.apiUrl}/v2/api/${this.apiPach}/cart/${id}`)
+                .then( (res)=> {
+
+                 
+
+                  
+                    alert(res.data.message);
+                     this.getCarsDataList();
+
+                })
+                .catch((er)=> {
+                    console.log(er);
+                } )
+
+               
+
+            },
+
+            claerAll(){
+
+                //https://vue3-course-api.hexschool.io/v2/api/kurokawa2021/carts
+
+                axios.delete(`${this.apiUrl}/v2/api/${this.apiPach}/carts`)
+                .then( (res)=> {
+                    console.log(res);
+                    alert(res.data.message);
+                    this.getCarsDataList();
+                })
+                .catch((er)=> {
+                    console.log(er);
+                } )
+
+
             }
     
     
@@ -50,6 +173,7 @@
         mounted(){
     
             this.getProductList();
+            this.getCarsDataList();
     
         }
     
@@ -57,23 +181,56 @@
     
         });
 
+        //元件
 
-
-        app.component( 'productmodal' , {
-
+        app.component( 'product-modal' , {
+            props:['id'],
             data(){
-                return{
+                return {
+                    apiUrl:'https://vue3-course-api.hexschool.io',
+                    apiPach:'kurokawa2021',
                     modal:'',
+                    product:{},
+                    qty:1
+
                 }
             },
-            template:'#userProductModal',
-            methdos:{
+            watch:{
 
-                openModal(){
-                   this.$emit( 'modal' );
+                //如果函式名稱id變動觸發
+
+                id(){
+                    this.getProduct();
                 }
 
-               
+            },
+          
+            template:'#userProductModal',
+            methods:{
+
+                opentheModalfn(){
+                    this.modal.show();                  
+                },
+                getProduct(){
+
+                    axios.get(`${this.apiUrl}/v2/api/${this.apiPach}/product/${this.id}`)
+                    .then( (res) => {
+        
+                        this.product = res.data.product;
+
+                        console.log(this.product);
+                       
+                        
+                    })
+                    .catch((er) => {
+                        console.log(er);
+                    });
+
+                },
+                addToCar(){
+                    this.$emit('add-car' , this.product.id , this.qty )
+                }
+
 
             },
             mounted(){
@@ -81,6 +238,9 @@
 
                 this.modal = new bootstrap.Modal(  this.$refs.modal  );
 
+               
+
+               
                
             }
 
